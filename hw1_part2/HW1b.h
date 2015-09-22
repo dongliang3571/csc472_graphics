6553d809f1d9d517d9067609154957f18439b274
Dong Liang <dongliang3571@gmail.com>
2015-09-22 00:52:19 -0700

    finished part2

--- hw1_part2/HW1b.h
+++ hw1_part2/HW1b.h
@@ -3,12 +3,22 @@
 
 #include "HW.h"
 
+typedef QVector2D vec2;
+typedef QVector3D vec3;
 
 class HW1b : public HW {
 	Q_OBJECT
 public:
 	HW1b(QWidget *parent = 0);
 	QGroupBox*	controlPanel();
+
+
+public slots:
+
+	void changeDividing(int value); //Slider calls his function to trigger dividing
+	void changeTheta(int angle);
+	void setToTwist(int state);
+
 
 	
 
@@ -17,22 +27,44 @@ protected:
 	void		resizeGL(int, int);
 	void		paintGL();
 
+
 private:
 
-	void divide_triangle(QVector2D a, QVector2D b, QVector2D c, int k);
-	void triangle(QVector2D a, QVector2D b, QVector2D c);
+	void divide_triangle(vec2 a, vec2 b, vec2 c, int k);
+	void triangle(vec2 a, vec2 b, vec2 c);
+	vec2 twisting(vec2 point);
+	void initPoint();
+	
 
 	int		 m_winW;
 	int		 m_winH;
-	int		 m_count=0;
+	float      ar;
+	float		 maxW;
+	float		 maxH;
 
+	int m_subdivide;
+	//bool m_changeColor;
 
-	QVector2D p[1000000] = {};
+	bool m_aspectCheck;
+	float m_theta;
+	
+	
+
+	std::vector<QVector2D> m_point;
+	std::vector<QVector3D> m_color;
+
+	
 
 
 	QSlider *m_sliderDividing;
 	QSpinBox *m_spinBoxDividing;
 	
+	QSlider *m_sliderTheta;
+	QSpinBox *m_spinBoxTheta;
+
+	QCheckBox *m_checkBoxTwist;
+	
+
 	
 
 };
